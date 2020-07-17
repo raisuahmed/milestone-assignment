21ac6680f77d3682ddc5928fc56fbcea94dc0049
raisuahmed <raisuahmed83@gmail.com>
2020-07-17 07:59:07 -0700

    finished the code

--- assignment.js
+++ assignment.js
@@ -1,7 +1,71 @@
+1.//feetToMile
+
 function feetToMile(feet) {
-    var mile = feet/5280;
-    return mile;
+    convertToMile = feet / 5280;
+    convertToMile = convertToMile.toFixed(4);
+    return convertToMile;
+    }
+//Check:
+Mile = feetToMile(5280);
+console.log(Mile);
+
+
+2.//woodCalculator
+
+function woodCalculator(chair, table , bed) { 
+var chairWoodFeet = chair * 1;
+var tableWoodFeet = table * 3;
+var bedWoodFeet = bed * 5;
+var totalWood = chairWoodFeet + tableWoodFeet + bedWoodFeet;
+return totalWood;
 }
-var mile = feetToMile(5280)
-console.log(mile);
+var woodResult = woodCalculator(14, 5, 12);
+console.log(" You need",woodResult, " cubic wood for all furniture");
+
+
+
+3.//brickCalculator 
+
+function brickCalculator(floor) { 
+    let perFeetBrick = 1000;
+    if(floor <= 0) {
+     console.log("A floor can't 0 or less then it");  
+    }
+    else if (floor <= 10){
+        brickIsNeeded = floor * 15 * perFeetBrick;
+        console.log("You need full of", brickIsNeeded, "bricks");
+        
+    }
+      else if (floor <= 20) {
+          brickIsNeeded = (10 * 15 * perFeetBrick) + ((floor-10) * 12 * perFeetBrick);
+          console.log("You need full of ", brickIsNeeded, "bricks");
+          
+      }
+      else {
+        brickIsNeeded = (10 * 15 * perFeetBrick) + (10 * 12 * perFeetBrick) + ((floor-20) * 10 * perFeetBrick);
+        console.log("You need full of ", brickIsNeeded, "bricks");
+    }
+}
+let floorIs = brickCalculator(23);
+   
+
+
+
+
+4.//tinyFriend Solution
+
+function tinyFriend(friendList){
+    var friend = friendList[0];
+    for (var i = 0; i < friendList.length; i++ ){
+        var currentPosition = friendList[i];
+            if (currentPosition.length < friend.length){
+            friend = currentPosition;
+        }
+    }
+        return friend;
+    }
+     
+
+ friendName=tinyFriend(["Rokon", "Liton", "Aminul", "Herok", "Soliman"]);
+console.log(friendName);
 
